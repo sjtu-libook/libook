80802cfa64fff3f8948ed1753a35721a293599c9
Alex Chi <iskyzh@gmail.com>
2021-03-09 09:16:17 +0800

    feat: add reservation query api

--- libookapi/serializers.py
+++ libookapi/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import RegionGroup, Region, Reservation
+from .models import RegionGroup, Region, Reservation, Timeslice
 
 
 class RegionGroupSerializer(serializers.ModelSerializer):
@@ -14,7 +14,40 @@ class RegionSerializer(serializers.ModelSerializer):
         fields = ('id', 'name', 'group', 'capacity')
 
 
-class ReservationSerializer(serializers.ModelSerializer):
+class TimesliceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Timeslice
+        fields = ('id', 'from_time', 'to_time')
+
+
+class ReservationDetailSerializer(serializers.ModelSerializer):
+    region = RegionSerializer(many=False, read_only=True)
+    time = TimesliceSerializer(many=False, read_only=True)
+
     class Meta:
         model = Reservation
         fields = ('id', 'region', 'time')
+
+
+class ReservationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reservation
+        fields = ('region', 'time')
+
+
+class ResultSerializer(serializers.Serializer):
+    success = serializers.BooleanField()
+    message = serializers.CharField(max_length=200)
+
+
+class RegionReservationSerializer(serializers.Serializer):
+    reserved = serializers.IntegerField()
+    time = TimesliceSerializer(many=False, read_only=True)
+    region = RegionSerializer(many=False, read_only=True)
+
+
+class RegionGroupReservationSerializer(serializers.Serializer):
+    reserved = serializers.IntegerField()
+    capacity = serializers.IntegerField()
+    time = TimesliceSerializer(many=False, read_only=True)
+    region_group = RegionGroupSerializer(many=False, read_only=True)
--- libookapi/views.py
+++ libookapi/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
-
+from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiExample
+from drf_spectacular.types import OpenApiTypes
 from rest_framework import viewsets, permissions, views, generics
-from rest_framework.decorators import action
-from .serializers import RegionGroupSerializer, RegionSerializer, ReservationSerializer
-from .models import RegionGroup, Region, Reservation
+from rest_framework.decorators import api_view
+
+from .serializers import *
+from .models import *
 
 
 class RegionGroupView(viewsets.ReadOnlyModelViewSet):
@@ -30,11 +32,54 @@ class ReservationView(viewsets.ReadOnlyModelViewSet):
     获取预约信息。
     """
     queryset = Reservation.objects.all()
-    serializer_class = ReservationSerializer
+    serializer_class = ReservationDetailSerializer
     filter_backends = [DjangoFilterBackend]
 
 
-class BatchReservationView(generics.ListAPIView):
+class QueryRegionReservationView(views.APIView):
+    @extend_schema(
+        parameters=[
+            OpenApiParameter("min_time", TimesliceSerializer),
+            OpenApiParameter("max_time", TimesliceSerializer),
+            OpenApiParameter("region_id", OpenApiTypes.INT,
+                             OpenApiParameter.PATH),
+        ],
+        responses=RegionReservationSerializer(many=True),
+    )
+    def get(self, request):
+        """
+        查询区域的预约情况
+        """
+        pass
+
+
+class QueryRegionGroupReservationView(views.APIView):
+    @extend_schema(
+        parameters=[
+            OpenApiParameter("min_time", TimesliceSerializer),
+            OpenApiParameter("max_time", TimesliceSerializer),
+            OpenApiParameter("region_group_id",
+                             OpenApiTypes.INT, OpenApiParameter.PATH),
+        ],
+        responses=RegionGroupReservationSerializer(many=True),
+    )
+    def get(self, request):
+        """
+        查询区域组的预约情况
+        """
+        pass
+
+
+class BatchReservationView(views.APIView):
+    queryset = Reservation.objects.all()
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter("min_time", TimesliceSerializer),
+            OpenApiParameter("max_time", TimesliceSerializer)
+        ],
+        responses=ReservationDetailSerializer(many=True),
+    )
     def get(self, request, format=None):
         """
         获取当前用户的所有预定信息。
@@ -42,19 +87,20 @@ class BatchReservationView(generics.ListAPIView):
         user = self.request.user
         return Reservation.objects.filter(user=user)
 
+    @extend_schema(
+        parameters=[
+            OpenApiParameter("delete", OpenApiTypes.BOOL)
+        ],
+        request=ReservationSerializer(many=True),
+        responses=ResultSerializer,
+    )
     def post(self, request, format=None):
         """
-        批量订位
+        批量订位/取消订位
         1. 用户只能在预约时间之前更改预约信息
         2. 一个场地的预约数不能超过场地限制
         3. 用户在同一个时间段只能预约一个场地
         4. ...
-        """
-        pass
-
-    def delete(self, request):
-        """
-        批量取消订位
         """
         pass
 
