cd76279d1e17a973100537962f06d41eea912d13
Alex Chi <iskyzh@gmail.com>
2021-03-22 22:27:52 +0800

    backend: refine reservation query API (#32)
    * backend: refine reservation query api
    
    * add to route
    
    * fix lint

--- libookapi/views/reservations.py
+++ libookapi/views/reservations.py
@@ -51,7 +51,37 @@ class QueryRegionReservationView(views.APIView):
     )
     def get(self, request):
         """
-        查询区域的预约情况
+        查询区域的预约情况，返回一个数组，代表该区域在 [min_time_id, max_time_id]
+        每个时间段内的预约情况。
+        """
+        region = request.GET.get('region_id')
+        min_time_id = request.GET.get('min_time_id')
+        max_time_id = request.GET.get('max_time_id')
+        reserved_time = Reservation.objects \
+            .filter(time__gte=min_time_id, time__lte=max_time_id, region=region) \
+            .values('region', 'time') \
+            .annotate(reserved=Count('*')) \
+            .annotate(region_id=F('region')) \
+            .annotate(time_id=F('time')) \
+            .values('region_id', 'time_id', 'reserved')
+        serializer = RegionReservationSerializer(reserved_time, many=True)
+        return Response(serializer.data)
+
+
+class QueryRegionReservationView(views.APIView):
+    @extend_schema(
+        parameters=[
+            OpenApiParameter("min_time_id", OpenApiTypes.INT),
+            OpenApiParameter("max_time_id", OpenApiTypes.INT),
+            OpenApiParameter("region_id", OpenApiTypes.INT,
+                             OpenApiParameter.PATH),
+        ],
+        responses=RegionReservationSerializer(many=True),
+    )
+    def get(self, request):
+        """
+        查询区域的预约情况，返回一个数组，代表该区域在 [min_time_id, max_time_id]
+        每个时间段内的预约情况。
         """
         region = request.GET.get('region_id')
         min_time_id = request.GET.get('min_time_id')
@@ -75,11 +105,12 @@ class QueryRegionGroupReservationView(views.APIView):
             OpenApiParameter("region_group_id",
                              OpenApiTypes.INT, OpenApiParameter.PATH),
         ],
-        responses=RegionGroupReservationSerializer(many=True),
+        responses=RegionReservationSerializer(many=True),
     )
     def get(self, request):
         """
-        查询区域组的预约情况
+        查询区域的预约情况，返回一个数组，代表在该区域组里的每一个区域在
+        [min_time_id, max_time_id] 每个时间段内的预约情况。
         """
         group = request.GET.get('region_group_id')
         min_time_id = request.GET.get('min_time_id')
@@ -93,6 +124,22 @@ class QueryRegionGroupReservationView(views.APIView):
             .values('region_id', 'time_id', 'reserved')
         serializer = RegionReservationSerializer(reserved_time, many=True)
         return Response(serializer.data)
+
+
+class QueryAllRegionGroupReservationView(views.APIView):
+    @extend_schema(
+        parameters=[
+            OpenApiParameter("min_time_id", OpenApiTypes.INT),
+            OpenApiParameter("max_time_id", OpenApiTypes.INT)
+        ],
+        responses=RegionGroupReservationSerializer(many=True),
+    )
+    def get(self, request):
+        """
+        查询所有区域组的预约情况，返回一个数组，代表各个区域组在
+        [min_time_id, max_time_id] 每个时间段内的预约情况。
+        """
+        return Response()
 
 
 class BatchReservationView(views.APIView):
