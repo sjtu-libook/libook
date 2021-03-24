6d942d6cf0144c701f00d553158e85a56192cafe
lzh-sjtu <1084388051@qq.com>
2021-03-24 08:44:38 +0800

    backend: add all region_group query and its test

--- libookapi/tests/test_reservation_query.py
+++ libookapi/tests/test_reservation_query.py
@@ -60,3 +60,46 @@ def test_reservation_region_group_query():
         {'reserved': 1, 'time_id': time2.id, 'region_id': region1.id},
         {'reserved': 1, 'time_id': time1.id, 'region_id': region2.id}
     ]
+
+
+@pytest.mark.django_db
+def test_all_reservation_region_group_query():
+    """可以查询某一时间的所有区域组预约情况"""
+    group1 = RegionGroup.objects.create(name="新图 1 楼")
+    group2 = RegionGroup.objects.create(name="新图 2 楼")
+    region1 = Region.objects.create(name="新图 E100", capacity=100, group=group1)
+    region2 = Region.objects.create(name="新图 A100", capacity=50, group=group1)
+    region3 = Region.objects.create(name="新图 E200", capacity=120, group=group2)
+    region4 = Region.objects.create(name="新图 A200", capacity=60, group=group2)
+    tz = timezone('Asia/Shanghai')
+    time1 = Timeslice.objects.create(from_time=tz.localize(
+        datetime(2021, 3, 11, 10)), to_time=tz.localize(datetime(2021, 3, 11, 11)))
+    time2 = Timeslice.objects.create(from_time=tz.localize(
+        datetime(2021, 3, 12, 10)), to_time=tz.localize(datetime(2021, 3, 12, 11)))
+
+    # Five users reserve in different ways
+    user = User.objects.create(username="AAA")
+    Reservation.objects.create(user=user, region=region1, time=time1)
+
+    user = User.objects.create(username="BBB")
+    Reservation.objects.create(user=user, region=region2, time=time1)
+
+    user = User.objects.create(username="CCC")
+    Reservation.objects.create(user=user, region=region2, time=time2)
+
+    user = User.objects.create(username="DDD")
+    Reservation.objects.create(user=user, region=region3, time=time1)
+
+    user = User.objects.create(username="EEE")
+    Reservation.objects.create(user=user, region=region4, time=time2)
+
+    client = Client()
+    response = client.get(f'/api/reservations/by_all',
+                          {'min_time_id': time1.id, 'max_time_id': time2.id})
+    assert response.status_code == 200
+    assert response.json() == [
+        {'reserved': 2, 'capacity': 150, 'time_id': time1.id, 'region_group_id': group1.id},
+        {'reserved': 1, 'capacity': 150, 'time_id': time2.id, 'region_group_id': group1.id},
+        {'reserved': 1, 'capacity': 180, 'time_id': time1.id, 'region_group_id': group2.id},
+        {'reserved': 1, 'capacity': 180, 'time_id': time2.id, 'region_group_id': group2.id}
+    ]
