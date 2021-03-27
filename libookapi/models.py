5c9ac736fe45049c440aa99cf79f9ec7c8124889
Alex Chi <iskyzh@gmail.com>
2021-03-27 14:03:09 +0800

    backend: add index for models (#51)
    * backend: add index for models
    
    * migrate

--- libookapi/models.py
+++ libookapi/models.py
@@ -29,6 +29,11 @@ class Region(models.Model):
         RegionGroup, related_name='regions', on_delete=models.CASCADE, help_text="区域所属区域组")
     capacity = models.IntegerField(help_text="区域最大可容纳人数")
 
+    class Meta:
+        indexes = [
+            models.Index(fields=['group']),
+        ]
+
 
 class Timeslice(models.Model):
     """
@@ -63,7 +68,8 @@ class Reservation(models.Model):
     class Meta:
         indexes = [
             models.Index(fields=['region', 'time']),
-            models.Index(fields=['user'])
+            models.Index(fields=['user']),
+            models.Index(fields=['time']),
         ]
 
 
