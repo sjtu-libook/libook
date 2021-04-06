fa16772f4ec79922c990ea2d7cede1e2385793ae
Alex Chi <iskyzh@gmail.com>
2021-04-06 16:09:50 +0800

    frontend: fix style and upgrade deps

--- booklib/src/SideBar.js
+++ booklib/src/SideBar.js
@@ -10,8 +10,8 @@ function SideBar() {
                 <ul className="navbar-nav me-auto">
                     <p className="navbar-brand">Libook</p>
                     <li className="nav-item">
-                        <NavLink className="nav-link" activeClassName="active" exact to="/">
-                            <i class="bi bi-calendar2-event"></i>&nbsp;&nbsp;所有预约
+                        <NavLink className="nav-link" activeClassName="active" to="/reservation/all">
+                            <i className="bi bi-calendar2-event"></i>&nbsp;&nbsp;所有预约
                         </NavLink>
                     </li>
                     <li className="nav-item">
@@ -19,7 +19,7 @@ function SideBar() {
                             className="nav-link"
                             activeClassName="active"
                             to="/reservation/custom">
-                            <i class="bi bi-calendar-plus"></i>&nbsp;&nbsp;自选预约
+                            <i className="bi bi-calendar-plus"></i>&nbsp;&nbsp;自选预约
                         </NavLink>
                     </li>
                     <li className="nav-item">
@@ -27,7 +27,7 @@ function SideBar() {
                             className="nav-link"
                             activeClassName="active"
                             to="/reservation/quick">
-                            <i class="bi bi-calendar-plus"></i>&nbsp;&nbsp;一键预约
+                            <i className="bi bi-calendar-plus"></i>&nbsp;&nbsp;一键预约
                         </NavLink>
                     </li>
                     <li className="nav-item">
@@ -35,7 +35,7 @@ function SideBar() {
                             className="nav-link"
                             activeClassName="active"
                             to="/population">
-                            <i class="bi bi-calendar-plus"></i>&nbsp;&nbsp;人流密度
+                            <i className="bi bi-calendar-plus"></i>&nbsp;&nbsp;人流密度
                         </NavLink>
                     </li>
                     <li className="nav-item">
@@ -43,11 +43,13 @@ function SideBar() {
                             className="nav-link"
                             activeClassName="active"
                             to="/contact-us">
-                            <i class="bi bi-calendar-plus"></i>&nbsp;&nbsp;联系我们
+                            <i className="bi bi-calendar-plus"></i>&nbsp;&nbsp;联系我们
                         </NavLink>
                     </li>
+                    <li className="nav-item my-3">
+                        <StatusBar></StatusBar>
+                    </li>
                 </ul>
-                <div className="my-3"><StatusBar></StatusBar></div>
             </div>
         </nav>
     </div>
