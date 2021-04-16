39f17bbaac2719a901c0e80e9a5dd0f0d174a90a
Alex Chi <iskyzh@gmail.com>
2021-04-16 13:42:03 +0800

    feat: refactor frontend

--- booklib/src/setupProxy.js
+++ booklib/src/setupProxy.js
@@ -1,10 +1,10 @@
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = function (app) {
-    app.use(
-        '/api',
-        createProxyMiddleware({
-            target: 'http://localhost:8000'
-        })
-    )
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:8000'
+    })
+  )
 }
