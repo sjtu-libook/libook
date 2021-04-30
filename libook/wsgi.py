eca23f57d66f01ecb113b24e8386eb41d2ca0fb7
Alex Chi <iskyzh@gmail.com>
2021-04-30 11:07:30 +0800

    deploy: fix netlify cookie

--- libook/wsgi.py
+++ libook/wsgi.py
@@ -13,4 +13,12 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'libook.settings')
 
-application = get_wsgi_application()
+django_app = get_wsgi_application()
+
+def netlify_fix_application(environ, start_response):
+    if "HTTP_COOKIE" in environ:
+        environ["HTTP_COOKIE"] = environ["HTTP_COOKIE"].replace(",", ";")
+    return django_app(environ, start_response)
+
+application = netlify_fix_application
+
